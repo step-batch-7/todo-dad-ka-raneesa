99aa004a1a0032af97b315bc51dce2a99767b00c
Step 25 <bhuvanadeepika017@gmail.com>
2020-02-12 19:43:15 +0530

    [deepika|shankar] modified search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,7 @@
+const selector = function(element) {
+  return document.querySelector(element);
+};
+
 const todoSearch = function(todoList) {
   const searchText = document.querySelector('#todoSearch').value;
   todoList.forEach(todo => {
@@ -18,20 +22,20 @@ const filterTasks = function(todo, searchText) {
 
 const unHighlightText = function(todo, tasks) {
   tasks.forEach(task => {
-    const text = document.querySelector(`[id='${todo.id}'] #task-${task.id} p`);
+    const text = selector(`[id='${todo.id}'] #task-${todo.id}-${task.id} p`);
     text.classList.remove('selected');
   });
 };
 
 const highlightMatchedTasks = function(todoId, tasks) {
   tasks.forEach(task => {
-    const text = document.querySelector(`[id='${todoId}'] #task-${task.id} p`);
+    const text = selector(`[id='${todoId}'] #task-${todoId}-${task.id} p`);
     text.classList.add('selected');
   });
 };
 
 const taskSearch = function(todoList) {
-  const searchText = document.querySelector('#taskSearch').value;
+  const searchText = selector('#taskSearch').value;
   todoList.forEach(todo => {
     unHighlightText(todo, todo.tasks);
     const matchedTasks = filterTasks(todo, searchText);
@@ -46,9 +50,9 @@ const taskSearch = function(todoList) {
 
 const clearSearchBar = function(event) {
   if (event.target.id === 'todoSearch') {
-    document.querySelector('#taskSearch').value = '';
+    selector('#taskSearch').value = '';
   } else {
-    document.querySelector('#todoSearch').value = '';
+    selector('#todoSearch').value = '';
   }
 };
 
