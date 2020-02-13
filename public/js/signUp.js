99538a67db4a914142f53a6b9e0af63b50f02aa5
Step 25 <bhuvanadeepika017@gmail.com>
2020-02-13 12:18:08 +0530

    [deepika|shankar] added signup page
    added signup.html and signup.css and signup.js
    added handler for signup
    added validation for input fields and
    created data file for saving the user details

---
+++ public/js/signUp.js
@@ -0,0 +1,37 @@
+const selector = function(element) {
+  return document.querySelector(element);
+};
+
+const sendXHR = function(method, url, message, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    if (this.status === 200) {
+      callback(this.responseText);
+    }
+  };
+  xhr.open(method, url);
+  xhr.send(message);
+};
+
+const callBack = function(text) {
+  const err = JSON.parse(text);
+  if (Object.keys(err).length === 0) {
+    window.location.href = window.location.href.split('/')[0] + '/login.html';
+  }
+  if (err.usernameError) {
+    selector('#usernameError').innerText = err.usernameError;
+  }
+  if (err.emailError) {
+    selector('#emailError').innerText = err.emailError;
+  }
+};
+
+const validateInput = function() {
+  selector('#usernameError').innerText = '';
+  selector('#emailError').innerText = '';
+  const inputs = Array.from(document.querySelectorAll('input'));
+  const userDetails = inputs.reduce((details, input) => {
+    return details + `${input.name}=${input.value}&`;
+  }, '');
+  sendXHR('POST', '/signup', userDetails.slice(0, -1), callBack);
+};
