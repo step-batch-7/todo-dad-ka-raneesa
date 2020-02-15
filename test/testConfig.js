48ca077e341e9ed2af97ae4cc988b60802f0f6a4
Step 25 <bhuvanadeepika017@gmail.com>
2020-02-15 14:19:13 +0530

    [deepika|shankar] added test cases
    created config files for handlers and test
    modified package.json
    created testUserDetails file

---
+++ test/testConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  usersDetails: '/Users/step25/projects/todo-dad-ka-raneesa/test/testUsersDetails.json',
+  todoLists: '/Users/step25/projects/todo-dad-ka-raneesa/test/testData.json',
+  sessions: [{
+    username: 'deepika',
+    SID: 12345
+  }]
+};
--- test/testHandlers.js
+++ test/testHandlers.js
@@ -12,15 +12,26 @@ describe('GET', () => {
     request(app)
       .get('/')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', /text\/html/)
       .expect(/TODO/, done);
+  });
+
+  it('Should get the signUp.html for "/templates/signUp.html" path', (done) => {
+    request(app)
+      .get('/signup')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .expect(/signUp/, done);
   });
 
   it('should get the index.css for "/css/index.css" path', done => {
     request(app)
       .get('/css/index.css')
       .set('accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', /text\/css/)
       .expect(/body {/, done);
@@ -28,13 +39,21 @@ describe('GET', () => {
 });
 
 describe('GET tasks', function() {
-  it('Should respond with all the tasks', function(done) {
+  it('Should respond with all the tasks if user is logged in', function(done) {
     request(app)
       .get('/tasks')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/deepika/, done);
+  });
+
+  it('Should respond 401 unauthorized if user not logged in', function(done) {
+    request(app)
+      .get('/tasks')
+      .set('Accept', '*/*')
+      .expect(401, done);
   });
 });
 
@@ -43,16 +62,26 @@ describe('POST createTodo', function() {
     request(app)
       .post('/createTodo')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .send('title=Shankar')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/Shankar/, done);
   });
 
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/createTodo')
+      .set('Accept', '*/*')
+      .send('title=Shankar')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/createTodo')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .send('name=Shankar')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
@@ -65,24 +94,37 @@ describe('POST removeTodo', function() {
     request(app)
       .post('/removeTodo')
       .send('id=2')
+      .set('Cookie', 'SID=12345')
       .set('Accept', '*/*')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/deepika/, done);
   });
+
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/removeTodo')
+      .set('Accept', '*/*')
+      .send('id=1')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/removeTodo')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .send('name=Shankar')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Bad Request/, done);
   });
+
   it('Should send not found if todo does not exist', function(done) {
     request(app)
       .post('/removeTodo')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .send('id=20')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
@@ -96,31 +138,44 @@ describe('POST createTask', function() {
       .post('/createTask')
       .send('id=1&work=noWork')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/noWork/, done);
   });
+
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/createTask')
+      .set('Accept', '*/*')
+      .send('id=1&work=noWork')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/createTask')
       .set('Accept', '*/*')
       .send('id=1')
+      .set('Cookie', 'SID=12345')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Bad Request/, done);
   });
+
   it('Should send not found if todo does not exist', function(done) {
     request(app)
       .post('/createTask')
       .set('Accept', '*/*')
       .send('id=20&work=noWork')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
   });
 });
 
-describe.only('POST removeTask', function() {
+describe('POST removeTask', function() {
   it('Should remove the task of given todo id and task id', function(done) {
     request(app)
       .post('/removeTask')
@@ -131,29 +186,43 @@ describe.only('POST removeTask', function() {
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/deepika/, done);
   });
+
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/removeTask')
+      .set('Accept', '*/*')
+      .send('todoId=1&taskId=2')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/removeTask')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .send('id=1')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Bad Request/, done);
   });
+
   it('Should send not found if todo does not exist', function(done) {
     request(app)
       .post('/removeTask')
       .set('Accept', '*/*')
       .send('todoId=20&taskId=1')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
   });
+
   it('Should send not found if task does not exist', function(done) {
     request(app)
       .post('/removeTask')
       .set('Accept', '*/*')
       .send('todoId=1&taskId=10')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
@@ -166,33 +235,48 @@ describe('POST changeStatus', function() {
       .post('/changeStatus')
       .send('todoId=1&taskId=1')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
-      .expect(/true/, done);
+      .expect(/deepika/, done);
   });
+
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/changeStatus')
+      .set('Accept', '*/*')
+      .send('todoId=1&taskId=1')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/changeStatus')
       .set('Accept', '*/*')
       .send('todoId=1')
+      .set('Cookie', 'SID=12345')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Bad Request/, done);
   });
+
   it('Should send not found if todo does not exist', function(done) {
     request(app)
       .post('/changeStatus')
       .set('Accept', '*/*')
       .send('todoId=20&taskId=1')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
   });
+
   it('Should send not found if task does not exist', function(done) {
     request(app)
       .post('/changeStatus')
       .set('Accept', '*/*')
       .send('todoId=1&taskId=20')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
@@ -205,24 +289,37 @@ describe('POST renameTodo', function() {
       .post('/renameTodo')
       .send('newTitle=Shankar&todoId=1')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/Shankar/, done);
   });
+
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/renameTodo')
+      .set('Accept', '*/*')
+      .send('newTitle=Shankar&todoId=1')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/renameTodo')
       .set('Accept', '*/*')
       .send('newTitle=Deepika')
+      .set('Cookie', 'SID=12345')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Bad Request/, done);
   });
+
   it('Should send not found if todo does not exist', function(done) {
     request(app)
       .post('/renameTodo')
       .set('Accept', '*/*')
       .send('newTitle=changed&todoId=20')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
@@ -235,15 +332,26 @@ describe('POST renameTask', function() {
       .post('/renameTask')
       .send('newTitle=Shankar&todoId=1&taskId=1')
       .set('Accept', '*/*')
+      .set('Cookie', 'SID=12345')
       .expect(200)
       .expect('Content-Type', CONTENT_TYPES.json)
       .expect(/Shankar/, done);
   });
+
+  it('Should respond with 401 if user not logged in', function(done) {
+    request(app)
+      .post('/renameTask')
+      .set('Accept', '*/*')
+      .send('newTitle=Shankar&todoId=1&taskId=1')
+      .expect(401, done);
+  });
+
   it('Should send bad request for invalid request', function(done) {
     request(app)
       .post('/renameTask')
       .set('Accept', '*/*')
       .send('newTitle=deepika')
+      .set('Cookie', 'SID=12345')
       .expect(400)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Bad Request/, done);
@@ -253,6 +361,7 @@ describe('POST renameTask', function() {
       .post('/renameTask')
       .set('Accept', '*/*')
       .send('newTitle=deepika&todoId=20&taskId=1')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
@@ -262,6 +371,7 @@ describe('POST renameTask', function() {
       .post('/renameTask')
       .set('Accept', '*/*')
       .send('newTitle=deepika&todoId=1&taskId=20')
+      .set('Cookie', 'SID=12345')
       .expect(404)
       .expect('Content-Type', CONTENT_TYPES.html)
       .expect(/Not Found/, done);
@@ -274,34 +384,35 @@ describe('POST signup', function() {
       .post('/signup')
       .set('Accept', '*/*')
       .send('name=shankar&email=sb@gmail.com&username=shankara&password=123456')
-      .expect(200)
-      .expect(/{}/, done);
+      .expect(302, done);
   });
-  it('Should send bad request for invalid request', function(done) {
-    request(app)
-      .post('/signup')
-      .set('Accept', '*/*')
-      .send('newTitle=deepika')
-      .expect(400)
-      .expect('Content-Type', CONTENT_TYPES.html)
-      .expect(/Bad Request/, done);
-  });
-  it('Should send bad request for invalid request', function(done) {
-    request(app)
-      .post('/signup')
-      .set('Accept', '*/*')
-      .send('newTitle=deepika')
-      .expect(400)
-      .expect('Content-Type', CONTENT_TYPES.html)
-      .expect(/Bad Request/, done);
-  });
-});
 
-describe('GET nonExisting Url', () => {
-  it('should return 404 for a non existing page', (done) => {
+  it('Should respond with 401 if user not logged in', function(done) {
     request(app)
-      .get('/badPage')
-      .expect(404, done)
+      .post('/signUp')
+      .set('Accept', '*/*')
+      .send('name=shankar&email=sb@gmail.com&username=shankara&password=123456')
+      .expect(200, done);
+  });
+
+  it('Should send bad request for invalid request', function(done) {
+    request(app)
+      .post('/signup')
+      .set('Accept', '*/*')
+      .send('newTitle=deepika')
+      .expect(400)
+      .expect('Content-Type', CONTENT_TYPES.html)
+      .expect(/Bad Request/, done);
+  });
+
+  it('Should send bad request for invalid request', function(done) {
+    request(app)
+      .post('/signup')
+      .set('Accept', '*/*')
+      .send('newTitle=deepika')
+      .expect(400)
+      .expect('Content-Type', CONTENT_TYPES.html)
+      .expect(/Bad Request/, done);
   });
 });
 
