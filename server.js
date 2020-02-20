dabf91035be9df804cdf3987226119645869a1b8
Step 25 <bhuvanadeepika017@gmail.com>
2020-02-20 12:18:51 +0530

    [deepika|shankar] added process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const app = require('./lib/routes');
 
-const defaultPort = 4000;
+const defaultPort = process.env.PORT || 4000;
 
 app.listen(defaultPort, () => {
   process.stdout.write(`Listening at port ${defaultPort}`);
